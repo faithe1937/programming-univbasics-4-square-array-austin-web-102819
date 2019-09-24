d749214f0fd143ec4d92d460803460f02ba723f9
Faith Janicek <faithe1937@gmail.com>
2019-09-24 18:32:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
  counter = 0 
 
 while counter < array.length do 
-  puts array [counter] ** 2 
+  puts array [counter]
   counter += 1 
 end
 end
 
-square_array([1,2,3,9,10,16,25])
+Math.sqrt.square_array([1,2,3,9,10,16,25])
